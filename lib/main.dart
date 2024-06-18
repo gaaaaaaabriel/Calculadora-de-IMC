2c40df1093c8ad70c66fc4f2a2354b4c184d8679
Israel Oliveira <israelolieira.dev@gmail.com>
2024-06-18 08:12:25 -0300

    Mudando os TextFields de funções para widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'package:calculadora_imc/buil_text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+
 
 void main() {
-  runApp(MaterialApp(
+  runApp( const MaterialApp(
     home: Home(),
   ));
 }
@@ -20,22 +20,8 @@ class _HomeState extends State<Home> {
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
 
-  Widget buildTextField(String label, TextEditingController controlador) {
-    return TextField(
-      decoration: InputDecoration(
-        labelText: label,
-        labelStyle:
-            TextStyle(color: Color.fromRGBO(85, 107, 47, 1), fontSize: 20.0),
-        border: OutlineInputBorder(),
-      ),
-      style: TextStyle(color: Color.fromRGBO(85, 107, 47, 1)),
-      keyboardType: TextInputType.number,
-      controller: controlador,
-    );
-  }
-
+  
   //Função para atualizar a tela, ela foi chamada
-  // pelo botão --> 'IconButton', linha 86.
   void resetFields() {
     setState(() {
       weightController.text = "";
@@ -45,7 +31,6 @@ class _HomeState extends State<Home> {
   }
 
   //Função para calcular o IMC, ela foi chamada pelo
-  // botão 'Verificar' -->(ElevatedButton) linha 112.
   void calculate() {
     double weight = double.parse(weightController.text);
     double height = double.parse(heightController.text) / 100;
@@ -73,15 +58,22 @@ class _HomeState extends State<Home> {
   }
 
   @override
+  void dispose() {
+    weightController.dispose();
+    heightController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromRGBO(85, 107, 47, 1),
-        title: Text("Calculadora de IMC"),
+        backgroundColor: const Color.fromRGBO(85, 107, 47, 1),
+        title: const Text("Calculadora de IMC"),
         centerTitle: true,
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.refresh),
+            icon: const Icon(Icons.refresh),
             onPressed: () {
               resetFields();
               //Aqui eu devo chaamar a função pra atualizar.
@@ -90,34 +82,34 @@ class _HomeState extends State<Home> {
         ],
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(10.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Icon(
+            const Icon(
               Icons.person_outline,
               size: 170.0,
               color: Color.fromRGBO(85, 107, 47, 1),
             ),
-            buildTextField("Peso", weightController),
-            Divider(),
-            buildTextField("Altura", heightController),
+            BuildTextField(label: "Peso", controller: weightController),
+            // buildTextField("Peso", weightController),
+            const Divider(),
+            BuildTextField(label: "Altura", controller: heightController),
+            // buildTextField("Altura", heightController),
             Padding(
               padding: const EdgeInsets.only(top: 10.0),
-              child: Container(
-                child: ElevatedButton(
-                  child: Text(
-                    "Verificar",
-                    style: TextStyle(color: Colors.white, fontSize: 25.0),
-                  ),
-                  onPressed: () {
-                    calculate();
-                    // logica aqui de verificação
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor:
-                        Color.fromRGBO(85, 107, 47, 1), // Cor de fundo do botão
-                  ),
+              child: ElevatedButton(
+                onPressed: () {
+                  calculate();
+                  // logica aqui de verificação
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:const
+                      Color.fromRGBO(85, 107, 47, 1), // Cor de fundo do botão
+                ),
+                child: const Text(
+                  "Verificar",
+                  style: TextStyle(color: Colors.white, fontSize: 25.0),
                 ),
               ),
             ),
@@ -125,7 +117,7 @@ class _HomeState extends State<Home> {
               padding: const EdgeInsets.only(top: 10.0),
               child: Text(
                 infoTexto,
-                style: TextStyle(
+                style: const TextStyle(
                     color: Color.fromRGBO(85, 107, 47, 1), fontSize: 25.0),
                 textAlign: TextAlign.center,
               ),
